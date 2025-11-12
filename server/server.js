0846c534a7d4d99eb864872167dc4db28a36c0a4
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-13 01:06:31 +0800

    feat: initialize server with Express and Morgan, add dotenv for environment variables

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan('dev'))
+
+app.listen(PORT, () => {
+  console.log(`Ready to build random sht`);
+});
