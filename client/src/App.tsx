c60d921ef53cdc62178748d096af2708892deb1c
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-16 01:27:24 +0800

    feat: add Button component with styles and integrate Prettier for code formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Button from './components/Button/Button'
 
 function App() {
 
   return (
     <>
-
+      <Button size='large' loading>Day 319</Button>
     </>
   )
 }
