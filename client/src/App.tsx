d9cdb0f28ab99ef8e9f7c308f473494f24c5bee5
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-17 01:04:34 +0800

    feat: implement routing for Login and Register components, add background SVG to body

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,61 @@
+import { Navigate, Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
-import Button from './components/Button/Button'
-import Card from './components/Card/Card'
-import Input from './components/Input/Input'
+import { AuthProvider } from './context/AuthContext'
+import PublicRoute from './components/PublicRoute/PublicRoute'
 import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
 
 function App() {
 
   return (
-    <>
-      <Card hover>
-        <Button size='large' loading>Day 319</Button>
-        <Input placeholder='Test' error='Oh No' label='Hell nah' fullWidth/>
-      </Card>
-      
-      <Login />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          {/* Public Routes Here */}
+          <Route 
+            path='/login'
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route 
+            path='/register'
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
 
-    </>
+          {/* Protected Rotues Here */}
+          <Route 
+            path='/dashboard'
+            element={
+              <PublicRoute>
+                {/* TODO: Dashboard */}
+                <Login />  
+              </PublicRoute>
+            }
+          />
+          <Route 
+            path='/stats'
+            element={
+              <PublicRoute>
+                {/* TODO: Stats */}
+                <Login />  
+              </PublicRoute>
+            }
+          />
+
+          {/* Default Rotues */}
+          <Route path='/' element={<Navigate to="/dashboard" replace />} />
+          <Route path='*' element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
